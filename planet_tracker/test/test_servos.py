2c64fb273f42957d9ba8c27122fd19bfe7d81f83
Max Cotton <maxcotton22@gmail.com>
2023-04-12 21:35:21 +0100

    Fix import path to servos

--- planet_tracker/test/test_servos.py
+++ planet_tracker/test/test_servos.py
@@ -1,11 +1,11 @@
 import unittest
-from planet_tracker.servos import Servos
+from planet_tracker.servos import PanTiltServos
 
 class TestServos(unittest.TestCase):
     def test_servo_limits(self):
-        servos = Servos()
-        self.assertEqual(servos.X_LIM, 90, "Servos X Limit should be 90")
-        self.assertEqual(servos.Y_LIM, 90, "Servos Y Limit should be 90")
+        panTiltServos = PanTiltServos()
+        self.assertEqual(panTiltServos.X_LIM, 90, "Servos X Limit should be 90")
+        self.assertEqual(panTiltServos.Y_LIM, 90, "Servos Y Limit should be 90")
 
 if __name__ == "__main__":
     unittest.main()
